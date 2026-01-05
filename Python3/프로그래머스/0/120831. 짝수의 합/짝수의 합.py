5bf1931b789d64de7d4490845d97297ba575765f
최성우 <cswcjw311427@naver.com>
2026-01-05 10:35:10 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 9.18 MB -BaekjoonHub

--- Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
+++ Python3/프로그래머스/0/120831. 짝수의 합/짝수의 합.py
@@ -1,7 +1,7 @@
 def solution(n):
     result = 0
-    for i in range(1,n+1):
-        if i%2 == 0:
-            result += i
-            
+    # range의 기능을 활용해 0부터 n+1까지 진행되는데 2씩 더해나가면서 진행됨
+    for i in range(0, n+1, 2):
+        result += i
+        
     return result
