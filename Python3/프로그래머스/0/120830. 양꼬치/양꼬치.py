dcc691c134318190bf13b085b9e7ce7bfea70972
최성우 <cswcjw311427@naver.com>
2026-01-05 10:29:38 +0900

    [level 0] Title: 양꼬치, Time: 0.01 ms, Memory: 9.1 MB -BaekjoonHub

--- Python3/프로그래머스/0/120830. 양꼬치/양꼬치.py
+++ Python3/프로그래머스/0/120830. 양꼬치/양꼬치.py
@@ -1,6 +1,11 @@
 def solution(n, k):
+    #양꼬치 총 먹은 갯수 * 가격
     yangPrice = n*12000
+    # 서비스 받은 음료수 갯수
     serviceDrink = int(n/10)
+    # 실제로 내야할 음료수 갯수
     paidDrink = k-serviceDrink
+    # 실제로 내야할 음료수 가격
     drinkPrice = paidDrink*2000
+    # 양꼬치 먹은갯수*가격 + 내야할 음료수 가격
     return yangPrice+drinkPrice
